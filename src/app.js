5284c8d6134e23d5dee2ccb80451c8d117b0fd54
francozeta <perufra@hotmail.com>
2024-02-26 15:11:27 -0500

    Added script to create default roles (user, moderator, admin)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
-import pkg from '../package.json'
+import pkg from '../package.json';
+
+import { createRoles } from './libs/initialSetup.js'
+
 import productsRoutes from './routes/products.routes.js';
+import authRoutes from './routes/auth.routes.js'
+
 const app = express();
+createRoles();
 
 app.set('pkg', pkg);
 
@@ -18,6 +24,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use('/products', productsRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
