4840066ba0d1d5f24c2b5891089730b0ac3f1d15
Franco <francozeta2011@gmail.com>
2024-02-27 12:55:56 -0500

    Implemented authentication in the application (sign in)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,5 +35,17 @@ export const signup = async (req, res) => {
 
 };
 export const signin = async (req, res) => {
-  res.json('signin')
+	const userFound = await User.findOne({email: req.body.email}).populate("roles");
+	if(!userFound) return res.status(400).json({message: 'User not found'});
+
+	const matchPassword = await User.comparePassword(req.body.password, userFound.password)
+	
+	if(!matchPassword) return res.status(401).json({token: null, message: 'Invalid password'});
+
+	const token = jwt.sign({id: userFound._id}, config.SECRET, {
+		expiresIn: 86400
+	})
+
+	console.log(userFound);
+	res.json({token});
 };
